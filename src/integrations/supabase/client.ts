0e780c11db0337d468ae7ff2590a994259cc419b
DarkGusta <cesarsilvavieira@gmail.com>
2025-06-03 21:54:34 +0100

    Fix: Not gonna say it cause I'm embarrassed

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,8 +3,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://fshdsmmyuvhqwngkzndm.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzaGRzbW15dXZocXduZ2t6bmRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczODkzMTIsImV4cCI6MjA2Mjk2NTMxMn0.6sgzv8A8x2nMgLOjAf44lXB72L70FxkhUbtrBml7onM";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
